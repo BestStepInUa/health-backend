de40594f3d33178109a2df84cf21a44fc82443fc
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2025-08-13 16:57:56 +0300

    user upd & del no work bag

--- src/user-profile/user-profile.controller.ts
+++ src/user-profile/user-profile.controller.ts
@@ -1,47 +1,69 @@
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import type { RequestWithUser } from 'src/auth/interfaces/requestWithUser.interface';
+
 import {
   Body,
   Controller,
   Delete,
-  Get,
-  Param,
+  // Get,
+  // Param,
   Patch,
-  Post,
+  Req,
+  UseGuards,
+  // Post,
 } from '@nestjs/common';
 
 import { UserProfileService } from './user-profile.service';
 
-import { CreateUserProfileDto } from './dto/create-user-profile.dto';
+// import { CreateUserProfileDto } from './dto/create-user-profile.dto';
 import { UpdateUserProfileDto } from './dto/update-user-profile.dto';
 
 @Controller('user-profile')
+@UseGuards(JwtAuthGuard)
 export class UserProfileController {
   constructor(private readonly userProfileService: UserProfileService) {}
 
-  @Post()
-  createUser(@Body() CreateUserProfileDto: CreateUserProfileDto) {
-    return this.userProfileService.createUser(CreateUserProfileDto);
-  }
+  // @Post()
+  // createUser(@Body() CreateUserProfileDto: CreateUserProfileDto) {
+  //   return this.userProfileService.createUser(CreateUserProfileDto);
+  // }
 
-  @Get(':id')
-  findUserById(@Param('id') id: number) {
-    return this.userProfileService.findUserById(id);
-  }
+  // @Get(':id')
+  // findUserById(@Param('id') id: number) {
+  //   return this.userProfileService.findUserById(id);
+  // }
 
-  @Get()
-  findAllUsers() {
-    return this.userProfileService.findAllUsers();
-  }
+  // @Get()
+  // findAllUsers() {
+  //   return this.userProfileService.findAllUsers();
+  // }
 
-  @Patch(':id')
+  // @Patch(':id')
+  // updateUserById(
+  //   @Param('id') id: number,
+  //   @Body() UpdateUserProfileDto: UpdateUserProfileDto,
+  // ) {
+  //   return this.userProfileService.updateUserById(id, UpdateUserProfileDto);
+  // }
+
+  // @Delete(':id')
+  // deleteUserById(@Param('id') id: number) {
+  //   return this.userProfileService.deleteUserById(id);
+  // }
+
+  @Patch()
   updateUserById(
-    @Param('id') id: number,
+    @Req() request: RequestWithUser,
     @Body() UpdateUserProfileDto: UpdateUserProfileDto,
   ) {
-    return this.userProfileService.updateUserById(id, UpdateUserProfileDto);
+    return this.userProfileService.updateUserById(
+      request.user.id,
+      UpdateUserProfileDto,
+    );
   }
 
-  @Delete(':id')
-  deleteUserById(@Param('id') id: number) {
-    return this.userProfileService.deleteUserById(id);
+  @Delete()
+  deleteUserById(@Req() request: RequestWithUser) {
+    return this.userProfileService.deleteUserById(request.user.id);
   }
 }
